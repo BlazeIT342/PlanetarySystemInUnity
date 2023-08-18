4891a64837511c064dcdb982f27682d3bfdaa5a0
BlazeIT342 <yltrazavr@gmail.com>
2023-08-18 21:26:37 +0300

    Add planet physics

---
+++ Assets/Scripts/Core/PlanetRotation.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Planetery.Core
+{
+    public class PlanetRotation : MonoBehaviour
+    {
+        [SerializeField] Transform orbitCenter;
+        [SerializeField] float rotationSpeed = 20f;
+        [SerializeField] float orbitRadius = 5f;
+
+        private void Update()
+        {
+            Vector3 newPosition = orbitCenter.position + Quaternion.Euler(0f, Time.time * rotationSpeed, 0f) * (Vector3.forward * orbitRadius);
+            transform.position = newPosition;
+        }
+    }
+}
